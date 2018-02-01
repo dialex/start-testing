46f10cbf0c0c912529f06aff407bcbb307e8a915
Diogo Nunes <dialex.nunes@gmail.com>
2018-02-01 10:52:27 +0000

    #39 debug output (sections)

--- ci.sh
+++ ci.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+echo '############################################################'
+echo '# Check Markdown files for linting issues'
+echo '############################################################'
+
 markdownlint docs --config docs/.markdownlint.json
