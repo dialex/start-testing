cd2a422f730f7e68539af2e31d5aa1ec96c99ec3
Diogo Nunes <dialex.nunes@gmail.com>
2018-02-01 11:00:14 +0000

    log all executed lines
    since it's a file, the contents were not being logged, so you could not know what was being executed

--- ci.sh
+++ ci.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+set -o xtrace
 
+# set +o xtrace
 echo '############################################################'
 echo '# Check Markdown files for linting issues'
 echo '############################################################'
+# set -o xtrace
 
 markdownlint docs --config docs/.markdownlint.json
+
+# set +o xtrace
+echo '############################################################'
+echo '# Check Markdown files for spelling mistakes'
+echo '############################################################'
+# set -o xtrace
