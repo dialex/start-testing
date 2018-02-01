55950c317cdf1d27310a002f7c287c955af8b893
Diogo Nunes <dialex.nunes@gmail.com>
2018-02-01 11:37:13 +0000

    Colored output

--- ci-helpers.sh
+++ ci-helpers.sh
@@ -1,12 +1,42 @@
 #!/bin/bash
 
+#################
+# Logging
+#################
+
 function log_task {
-    echo ""
-    echo "### " "${1}"
-    echo ""
+    echo_blue ""
+    echo_blue "### ${1}"
+    echo_blue ""
 }
 
 function run_step {
-    echo "${1}"
+    echo_yellow "${1}"
     eval "${1}"
+}
+
+#################
+# Colored output
+#################
+
+red=`tput setaf 1`
+green=`tput setaf 2`
+yellow=`tput setaf 3`
+blue=`tput setaf 4`
+reset=`tput sgr0`
+
+function echo_red {
+    echo "${red}${1}${reset}"
+}
+
+function echo_green {
+    echo "${green}${1}${reset}"
+}
+
+function echo_yellow {
+    echo "${yellow}${1}${reset}"
+}
+
+function echo_blue {
+    echo "${blue}${1}${reset}"
 }
