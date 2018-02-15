3352d3f389fea620c2194454fced45ef504779bc
Diogo Nunes <dialex.nunes@gmail.com>
2018-02-15 11:55:05 +0000

    install tool (still needs ruby)

--- ci-install.sh
+++ ci-install.sh
@@ -5,3 +5,5 @@ source ./ci-helpers.sh
 
 log_task "Install tool dependencies"
 run_step "npm i -g markdownlint-cli markdown-spellcheck remark-cli remark-validate-links"
+run_step "npm i -g markdownlint-cli markdown-spellcheck"
+run_step "gem install --no-ri --no-rdoc awesome_bot"
