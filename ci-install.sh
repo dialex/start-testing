83150c0e4528c00d00b8f7ea1b8afbb3bdc8fd41
Diogo Nunes <dialex.nunes@gmail.com>
2018-02-15 17:00:03 +0000

    separate JS and Ruby dependencies

--- ci-install.sh
+++ ci-install.sh
@@ -5,5 +5,8 @@ source ./ci-helpers.sh
 
 log_task "Install tool dependencies"
 run_step "npm i -g markdownlint-cli markdown-spellcheck remark-cli remark-validate-links"
+log_task "Install JavaScript tool dependencies"
 run_step "npm i -g markdownlint-cli markdown-spellcheck"
+
+log_task "Install Ruby tool dependencies"
 run_step "gem install --no-ri --no-rdoc awesome_bot"
--- ci-run.sh
+++ ci-run.sh
@@ -9,6 +9,7 @@ run_step "markdownlint docs --config docs/.markdownlint.json"
 log_task 'Check for spelling mistakes'
 run_step "mdspell 'docs/**/*.md' -r --en-gb --ignore-numbers --ignore-acronyms"
 
+log_task 'Check for broken external links'
 log_task 'Check for broken internal links'
 run_step "remark -u validate-links docs/ --frail"
 
