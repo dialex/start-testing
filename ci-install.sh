3c2d1be81067c3aceb594887d4e9c0f56f8e6558
Diogo Nunes <dialex.nunes@gmail.com>
2018-02-16 10:21:52 +0000

    update with master

--- ci-install.sh
+++ ci-install.sh
@@ -3,10 +3,8 @@
 
 source ./ci-helpers.sh
 
-log_task "Install tool dependencies"
-run_step "npm i -g markdownlint-cli markdown-spellcheck remark-cli remark-validate-links"
 log_task "Install JavaScript tool dependencies"
-run_step "npm i -g markdownlint-cli markdown-spellcheck"
+run_step "npm i -g markdownlint-cli markdown-spellcheck remark-cli remark-validate-links"
 
 log_task "Install Ruby tool dependencies"
 run_step "gem install --no-ri --no-rdoc awesome_bot"
--- ci-run.sh
+++ ci-run.sh
@@ -9,9 +9,8 @@ run_step "markdownlint docs --config docs/.markdownlint.json"
 log_task 'Check for spelling mistakes'
 run_step "mdspell 'docs/**/*.md' -r --en-gb --ignore-numbers --ignore-acronyms"
 
-log_task 'Check for broken external links'
 log_task 'Check for broken internal links'
 run_step "remark -u validate-links docs/ --frail"
 
-log_task 'Check for broken links'
+log_task 'Check for broken external links'
 run_step "awesome_bot --allow-redirect --allow-dupe --skip-save-results docs/**/*.md"
