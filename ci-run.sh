c8a8d54c2c5af599f0bc2abae51531933f937cbc
Diogo Nunes <dialex.nunes@gmail.com>
2018-12-24 11:45:22 +0000

    refactor lint:markdown

--- ci-run.sh
+++ ci-run.sh
@@ -4,9 +4,6 @@
 set -e
 source ./ci-helpers.sh
 
-log_task "Check for linting issues"
-run_step "markdownlint docs --config docs/.markdownlint.json"
-
 log_task 'Check for spelling mistakes'
 run_step "mdspell 'docs/**/*.md' -r --en-gb --ignore-numbers --ignore-acronyms"
 
