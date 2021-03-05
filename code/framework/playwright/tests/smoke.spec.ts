3d51383df6d6162a04f642c3729f025837245f99
Diogo Nunes <dialex.nunes@gmail.com>
2021-03-05 13:51:19 +0000

    apparently playwright-preset allows me to omit the setup source https://www.carlrippon.com/getting-started-with-playwright/

--- code/framework/playwright/tests/smoke.spec.ts
+++ code/framework/playwright/tests/smoke.spec.ts
@@ -1,25 +1,6 @@
-const { chromium } = require("playwright");
-
 const AMAZON_HOME = "https://amazon.com/";
 
 describe("Amazon", function () {
-  // ########### SETUP ###########
-  beforeAll(async () => {
-    browser = await chromium.launch();
-  });
-  afterAll(async () => {
-    await browser.close();
-  });
-  beforeEach(async () => {
-    context = await browser.newContext();
-    page = await context.newPage();
-  });
-  afterEach(async () => {
-    await context.close();
-    await page.close();
-  });
-  // #############################
-
   it("should be online", async () => {
     await page.goto(AMAZON_HOME);
     expect(page).not.toBeNull();
